a1a23ab2c89b6c2fdc9f7e51c66bd5aea4cf1761
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-09-23 21:12:26 -0500

    initial commit

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -1,3 +1,36 @@
-export function myFunc(param: string): string {
-  return param;
+import { useEffect, useRef } from 'react';
+
+interface ChannelsCollection {
+  [namespace: string]: BroadcastChannel;
 }
+
+class ChannelStore {
+  channels: ChannelsCollection = {};
+
+  add = (namespace: string): BroadcastChannel => {
+    if (this.channels[namespace]) return;
+    const bc: BroadcastChannel = new BroadcastChannel(namespace);
+    this.channels[namespace] = bc;
+    return bc;
+  };
+}
+
+const channelStore: ChannelStore = new ChannelStore();
+
+function useSync(signature: [any, (any) => any], namespace: string): [any, (any) => any] {
+  const [val, setVal] = signature;
+  const channel: any = useRef();
+
+  useEffect(() => {
+    const bc = channelStore.add(namespace);
+    channel.current = bc;
+  }, []);
+
+  // TODO: broadcast messages each time state changes
+
+  // TODO: fire local setter any time new postMessage comes in
+
+  return [val, setVal];
+}
+
+export default useSync;
--- test/app/index.js
+++ test/app/index.js
@@ -1,3 +1,16 @@
-import * as lib from '../../src/lib'
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import useSync from '../../src/lib';
 
-console.log(lib.myFunc('hello world'))
+function App() {
+  const [clicked, setClicked] = useSync(useState(0), 'clicked');
+
+  return (
+    <React.Fragment>
+      <p>Clicked {clicked} times!</p>
+      <button onClick={() => setClicked(clicked + 1)}>Click</button>
+    </React.Fragment>
+  );
+}
+
+render(<App />, document.getElementById('app'));
