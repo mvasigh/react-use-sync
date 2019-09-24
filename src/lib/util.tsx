46f1412a27545a47fbc3ec729b62ee2a239b0f64
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-09-24 17:14:16 -0500

    add utility functionsÆ

---
+++ src/lib/util.tsx
@@ -0,0 +1,18 @@
+export const debounce = (func: (any) => any, delay: number = 100): ((any) => any) => {
+  let timer;
+  return function() {
+    const context = this;
+    const args = arguments;
+    clearTimeout(timer);
+    timer = setTimeout(() => func.apply(context, args), delay);
+  };
+};
+
+// https://stackoverflow.com/a/2117523/11599918
+export const uuid = (): string => {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+};
