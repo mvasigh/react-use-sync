77c92c8f542c4cc8627d15e4c294cacee3b8c9c1
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-09-24 17:13:53 -0500

    WIP work on index

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -1,31 +1,49 @@
 import { useEffect, useRef } from 'react';
+import SyncChannel from './SyncChannel';
 
 interface ChannelsCollection {
-  [namespace: string]: BroadcastChannel;
+  [namespace: string]: SyncChannel;
 }
 
 class ChannelStore {
   channels: ChannelsCollection = {};
 
-  add = (namespace: string): BroadcastChannel => {
-    if (this.channels[namespace]) return;
-    const bc: BroadcastChannel = new BroadcastChannel(namespace);
-    this.channels[namespace] = bc;
-    return bc;
+  formatNamespace = (namespace: string): string => {
+    return `react-use-sync:${namespace}`;
+  };
+
+  subscribeTo = (namespace: string): SyncChannel => {
+    const formattedName = this.formatNamespace(namespace);
+    let syncChannel: SyncChannel = this.channels[formattedName] || null;
+    if (!syncChannel) {
+      syncChannel = this.createChannel(formattedName);
+    }
+    // subscribe to the sync channel
+    // ...
+    return syncChannel;
+  };
+
+  createChannel = (formattedName: string): SyncChannel => {
+    const syncChannel = new SyncChannel(formattedName);
+    this.channels[formattedName] = syncChannel;
+    return this.channels[formattedName];
   };
 }
 
 const channelStore: ChannelStore = new ChannelStore();
 
+function useChannel(namespace: string): SyncChannel {
+  const channelRef: any = useRef({ id: null, channel: null });
+  useEffect(() => {
+    // const channel = channelStore.add(namespace);
+    // channelRef.current = channel;
+  }, []);
+  return channelRef.current;
+}
+
 function useSync(signature: [any, (any) => any], namespace: string): [any, (any) => any] {
   const [val, setVal] = signature;
-  const channel: any = useRef();
-
-  useEffect(() => {
-    const bc = channelStore.add(namespace);
-    channel.current = bc;
-  }, []);
-
+  const channel = useChannel(namespace);
   // TODO: broadcast messages each time state changes
 
   // TODO: fire local setter any time new postMessage comes in
