6a8d6a911d146cc4c3ffb9392a18bd52eaf384ce
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-10-01 21:07:35 -0500

    build lib before using in dev application

--- test/app/index.js
+++ test/app/index.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { render } from 'react-dom';
-import useSync from '../../src/lib';
+import useSync from '../../build';
+
+console.log({ useSync });
 
 function App() {
-  const [clicked, setClicked] = useSync(useState(0), 'clicked');
+  const [clicked, setClicked] = useSync('clicked', useState(0));
 
   return (
     <React.Fragment>
